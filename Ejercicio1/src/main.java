fae353ff5513c23423960a8b6bff20fbc6b1bc2c
ALBERTOBALAGUEPADRON <usr2276@salesianos-lacuesta.net>
2021-11-01 16:35:51 +0000

    Feature: Introduzco el ProcessBuilder, y pongo la dirección del ejecutable, Tambien ejecuto un bucle que habre las ventanas que me interesan, y añado cada proceso a un arrayList de procesos

---
+++ Ejercicio1/src/main.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        ventanas(5);
+
+    }
+
+    public static  void ventanas(int num){
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        ArrayList<Process> ListProcess = new ArrayList<>();
+
+        try {
+            for ( int i = 0; i < num; i++) {
+                ListProcess.add(processBuilder.start());
+                System.out.println(ListProcess.size());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
